49e38db505011a75be020ca09f7642274892112a
hendri2212 <cowok_cool320@yahoo.co.id>
2024-05-17 19:31:23 +0800

    Update CompanySeeder to Eloquent

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Company;
 
 class CompanySeeder extends Seeder
 {
@@ -15,28 +16,18 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('companies')->insert([
-            [
-                'name' => 'SMPN 1 Kotabaru',
-            ],
-            [
-                'name' => 'SMPN 2 Kotabaru',
-            ],
-            [
-                'name' => 'SMPN 4 Kotabaru',
-            ],
-            [
-                'name' => 'SMPN 5 Kotabaru',
-            ],
-            [
-                'name' => 'SMPN 6 Kotabaru',
-            ],
-            [
-                'name' => 'MTSN 2 Kotabaru',
-            ],
-            [
-                'name' => 'MTS RAUDHATUL JANNAH',
-            ]
-        ]);
+        $companies = [
+            'SMPN 1 Kotabaru',
+            'SMPN 2 Kotabaru',
+            'SMPN 4 Kotabaru',
+            'SMPN 5 Kotabaru',
+            'SMPN 6 Kotabaru',
+            'MTSN 2 Kotabaru',
+            'MTS RAUDHATUL JANNAH'
+        ];
+        
+        foreach ($companies as $company) {
+            Company::create(['name' => $company]);
+        }        
     }
 }
